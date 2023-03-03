64bd98a53a04026b97e81b34d0ab47861abed08f
qichang.dong <qichang.dong@videt.cn>
2023-03-04 06:26:32 +0800

    u

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
+[ ! -d zlog ] && mkdir -p zlog
 nohup sh run_alphafold.sh \
--d data \
--o output \
+-d /home/qcdong/af_data \
+-o /mnt/sdc/af_out \
 -p monomer_ptm \
--i input/GA98.fasta \
--t 1800-01-01 \
+-i input/mono_set1/GA98.fasta \
+-t 2022-01-01 \
 -m model_1 \
 -f \
 > run.log 2>&1 &
