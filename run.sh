c04fa45840b10e5d9baf2220a0fb93733b90a0b7
qichang.dong <qichang.dong@videt.cn>
2023-03-06 21:39:29 +0800

    u

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 [ ! -d zlog ] && mkdir -p zlog
-nohup sh run_alphafold.sh \
+
+out_dir=/mnt/sdc/af_out/test
+input_dir=input/mono_set1
+input_file=GA98.fasta
+[ ! -d $out_dir ] && mkdir -p $out_dir
+
+out_dir=/mnt/sdc/af_out
+input_dir=/mnt/sdc/af_input
+input_file=5qtu_A_H_pdb_fasta_pos_human.fasta
+
+nohup bash run_alphafold.sh \
 -d /home/qcdong/af_data \
--o /mnt/sdc/af_out \
--p monomer_ptm \
--i input/mono_set1/GA98.fasta \
+-o $out_dir \
+-p multimer \
+-i $input_dir/$input_file \
 -t 2022-01-01 \
--m model_1 \
--f \
-> run.log 2>&1 &
+> $input_file.log 2>&1 &
